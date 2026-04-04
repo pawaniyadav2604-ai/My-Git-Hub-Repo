f9b38f4da75a16a738f7e79bca9e7d528d5348b8
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-04 22:39:38 +0530

    added Arrays.java

---
+++ Backtracking/Arrays.java
@@ -0,0 +1,27 @@
+package Backtracking;
+
+public class Arrays {
+    public static void changeArr(int arr[],int index, int value ){
+        //// base case 
+        if (index== arr.length){
+            printArr(arr);
+            return;
+        }
+
+        ///// recursion 
+        arr[index] = value;
+        arr[index] = arr[index] -2;
+    }
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
