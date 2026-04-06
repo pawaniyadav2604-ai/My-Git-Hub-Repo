2f2884955c826af43cfe1434986ca1a936a698aa
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-06 22:35:30 +0530

    Update Question11.java

--- Arrays/Question11.java
+++ Arrays/Question11.java
@@ -1,3 +1,39 @@
-public class Question11 {
-    
-}
+import java.util.Scanner;
+
+class Question11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Input elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                secondMin = min;
+                min = arr[i];
+            } 
+            else if (arr[i] < secondMin && arr[i] != min) {
+                secondMin = arr[i];
+            }
+        }
+
+        if (secondMin == Integer.MAX_VALUE) {
+            System.out.println("No second smallest element found");
+        } else {
+            System.out.println("Second smallest element in: " + secondMin);
+        }
+    }
+} 
+
+
