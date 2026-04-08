695e8bd818312497aa78e8b348362ef303dc2d2c
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-08 22:59:07 +0530

    Create Question14.java

---
+++ Arrays/Question14.java
@@ -0,0 +1,9 @@
+public class Question14 {
+    public static void main(String[] args) {
+        char arr[] ={'A', 'B' ,'C', 'D'};
+        System.out.println("Array elements :");
+        for (char c : arr) {
+            System.out.println(c);
+        }
+    }
+}
