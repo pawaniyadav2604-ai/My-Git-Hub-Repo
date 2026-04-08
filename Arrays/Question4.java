467adec77e5022a5b136b62a090ae0ed2ce78b99
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-08 19:14:17 +0530

    added Question4.java

--- Arrays/Question4.java
+++ Arrays/Question4.java
@@ -1,45 +1,38 @@
 import java.util.*;
 
 class Question4 {
-
-    public static int deleteElement(int[] arr, int n, int key) {
-        int i;
-
-        // find the element
-        for (i = 0; i < n; i++) {
-            if (arr[i] == key) {
+    public static int removeelement(int arr[] , int n , int value){
+        int index= -1;
+        
+        for(int i =0 ;i< n; i++){
+            if (arr[i] == value){
+                index = i;
                 break;
             }
         }
-
-        // if element not found
-        if (i == n) {
-            System.out.println("Element not found");
-            return n;
+        if(index != -1) {
+        for (int i = index ; i< n-1 ; i++){
+            arr[i] = arr[i+1];
         }
-
-        // shift elements left
-        for (int j = i; j < n - 1; j++) {
-            arr[j] = arr[j + 1];
-        }
-
-        return n - 1; // new size
+        n--;
+    }else {
+        System.out.print(" Element not found. ");
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int arr[] = {10, 20, 30, 40, 50};
-        int n = arr.length;
-
-        System.out.print("Enter element to delete: ");
-        int key = sc.nextInt();
-
-        n = deleteElement(arr, n, key);
-
-        System.out.println("Array elements after deleting the element:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+    return n;
+    }
+    
+public static void main(String[] args) {
+    int arr[] = {1,0,2,0,4,0,5,0,6,0,7,0};
+    int n = arr.length;
+    
+    Scanner sc= new Scanner (System.in);
+    System.out.print("enter the element you want to delete :");
+    int value = sc.nextInt();
+    
+       n = removeelement(arr, n, value);
+       System.out.print(" the array after deleting the element :");
+       for(int i=0 ; i<n; i++){
+           System.out.print(arr[i] + " ");
+       }
     }
 }
