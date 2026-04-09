e0e7beb244d36da9d549bf166c3a17646ad38b40
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-09 22:13:12 +0530

    add

---
+++ String/Basics.java
@@ -0,0 +1,18 @@
+package String;
+
+public class Basics {
+    public static void main(String[] args) {
+        // String str = "pawani";
+        // System.out.println(str);
+
+        ////// to print length 
+        String str = "pawani";
+        int l = str.length();
+        System.out.println(l);
+        
+        //System.out.println(str.contains("i"));
+
+        System.out.println(str.startsWith("p"));
+        System.out.println(str.endsWith("i"));
+    }
+}
---
+++ String/ContinuouspartofString.java
@@ -0,0 +1,8 @@
+package String;
+
+public class ContinuouspartofString {
+    public static void main(String[] args) {
+        String str = "pakhi";
+        System.out.println(str.substring(2, 5));
+    }
+}
