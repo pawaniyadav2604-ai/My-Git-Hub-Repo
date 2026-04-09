84037eb55c93ff4e913aebac44863f761a528e59
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-09 22:42:15 +0530

    Update ContinuouspartofString.java

--- String/ContinuouspartofString.java
+++ String/ContinuouspartofString.java
@@ -2,7 +2,21 @@ package String;
 
 public class ContinuouspartofString {
     public static void main(String[] args) {
-        String str = "pakhi";
-        System.out.println(str.substring(2, 5));
+        // String str = "pakhi";
+        // System.out.println(str.substring(2, 5));
+
+        // String s = "physics";
+        // for (int i = 2; i < 4; i++) {
+        //     System.out.println(s.substring(i));
+        // }
+
+        // print all substring 
+
+        String str = "abcd";
+        for (int i = 0; i <=3; i++) {
+            for (int j = i+1; j <=4; j++) {
+                System.out.print(str.substring(i,j) + " ");
+            }
+        }
     }
 }
