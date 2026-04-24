451c7db09d407360a95c4df0ba9bc529c4b1324a
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-24 20:50:39 +0530

    added

---
+++ Arraylist/sizeofarraylist.java
@@ -0,0 +1,20 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class sizeofarraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+       // System.out.println(list);
+        //System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
