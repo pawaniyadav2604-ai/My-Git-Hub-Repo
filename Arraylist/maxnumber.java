a361acb4536b3885ac5a89ce120a5228a4937d1c
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-24 23:54:49 +0530

    Create maxnumber.java

---
+++ Arraylist/maxnumber.java
@@ -0,0 +1,25 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class maxnumber {
+     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(7);
+        list.add(8);
+       System.out.println(list);
+       int max=  Integer.MIN_VALUE;
+
+       for (int index = 0; index < list.size(); index++) {
+          if(max < list.get(index)){
+               max = list.get(index);
+          }
+       }
+       System.out.println("Max element is : " + max);
+
+     }
+}
