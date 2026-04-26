8276025807409207e43bf6ecb52f15d71cb8348c
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-26 14:02:36 +0530

    added

---
+++ Arraylist/Containerwithmostwater.java
@@ -0,0 +1,34 @@
+ package Arraylist;
+
+import java.util.ArrayList;
+
+class Containerwithmostwater {
+    public static int storewater(ArrayList<Integer> height){
+        int maxwater = 0;
+        // bruteforce 
+
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i+1; j < height.size(); j++) {
+                int ht = Math.min(height.get(i), height.get(j));
+                int width = j-1;
+                int currentwater = ht * width;
+                maxwater = Math.max(maxwater, currentwater);
+            }
+        }
+        return maxwater;
+    }
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(5);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(storewater(height));
+    }
+}
