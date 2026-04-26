42c9f50f4b830200c69a2e32704529cbfa7136e4
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-04-26 23:38:55 +0530

    Create Basics.java

---
+++ LinkedList/Basics.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Basics {
+    public static class  nodes {
+        int data ;
+         nodes next;
+
+         public nodes(int data){
+            this.data = data;
+            this.next = null;
+         }
+    }
+    public static nodes head;
+    public static nodes tail ;
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList<>();
+        
+        
+    }
+}
+ 
