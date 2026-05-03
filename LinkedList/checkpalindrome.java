5c6352baccb2e14d248123fd51be4fa5f2c16d05
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-05-03 17:00:58 +0530

    Update checkpalindrome.java

--- LinkedList/checkpalindrome.java
+++ LinkedList/checkpalindrome.java
@@ -52,8 +52,52 @@ public class checkpalindrome {
         }
         System.out.println("null");
     }
+   //// slow-fast approach 
+    public nodes findmid(nodes head){
+        nodes slow = head ;
+        nodes fast = head;
+
+        while (fast != null  && fast.next != null) {
+            slow = slow.next; //+1
+            fast = fast.next.next; //+2
+
+        }
+
+         return slow;
+    }
 
 
+    public boolean palindrome(){
+        if (head == null && head.next == null){
+            return true;
+        }
+        // step 1 - find mid 
+        nodes midnode = findmid(head);
+
+        //step 2 - reverse 2nd half
+       nodes prev = null;
+        nodes curr = midnode;
+        nodes next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        nodes right = prev; // right ka head 
+        nodes left = head;
+
+        // step 3 - check left and right half
+        while (right != null) {
+            if (left.data != right.data){
+                return false;
+            }
+            left =left.next;
+            right = right.next;
+        }
+        return true;
+    }
    
 
     public static void main(String[] args) {
@@ -62,10 +106,12 @@ public class checkpalindrome {
 
         ll.addfirst(2);
         ll.addfirst(1);
-        ll.addLast(3);
-        ll.addLast(4);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.addLast(5);
 
         ll.print();
+        System.out.println(ll.palindrome());
        
     }
 }
