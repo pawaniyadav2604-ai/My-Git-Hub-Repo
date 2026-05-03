9bdef932b2d1613c85827650c628dfe26320bc5d
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-05-03 21:59:00 +0530

    Create detectaloop.java

---
+++ LinkedList/detectaloop.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class detectaloop {
+    public static class nodes {
+        int data;
+        nodes next;
+
+        public nodes(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static nodes head;
+    public static nodes tail;
+
+    public void addfirst(int data) {
+        nodes newNode = new nodes(data);
+
+        if (head == null) {   // ✅ fixed
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        nodes newNode = new nodes(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+
+        nodes temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        detectaloop ll = new detectaloop();
+ 
+        ll.addfirst(2);
+        ll.addfirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        ll.print();
+
+       
+    }
+}
