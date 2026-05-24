58fd6c643555e93b4503eb75b0bc95e2827d9fdf
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-05-24 17:27:51 +0530

    added

---
+++ LinkedList/DoublyLL.java
@@ -0,0 +1,78 @@
+package LinkedList;
+
+public class DoublyLL {
+    public static class  Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data){
+        this.data = data;
+        this.next= null;
+        this.prev = null;
+    }
+}
+        public static Node head;
+        public static Node tail;
+        public static int size;
+
+        //add
+        public void addFirst(int data){
+            Node newnode = new Node(data);
+            size++;
+            if (head == null){
+                head = tail = newnode;
+                return;
+            }
+
+            newnode.next =head;
+            head.prev = newnode;
+            head = newnode;
+        }
+        
+
+        //print 
+        public void print(){
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data + "<->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+
+         
+        //remove
+        public int removefirst(){
+            if (head == null){
+                System.out.println( "dl is empty");
+                return Integer.MIN_VALUE;
+            }
+
+            if (size == 1) {
+                int val = head.data;
+                head = tail = null;
+                size--;
+                return val;
+            }
+
+            int val = head.data;
+            head = head.next;
+            head.prev = null;
+            size--;
+            return val;
+        }
+
+        public static void main(String[] args) {
+            DoublyLL dll = new DoublyLL();
+            dll.addFirst(9);
+            dll.addFirst(8);
+            dll.addFirst(7);
+            dll.print();
+
+            dll.removefirst();
+            dll.print();;
+            System.out.println(dll.size);
+        }
+   
+}
