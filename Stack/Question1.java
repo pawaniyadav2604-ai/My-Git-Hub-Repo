978e7916856405226c4f96e71ed49f85dc21f939
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-05-27 17:42:14 +0530

    Create Question1.java

---
+++ Stack/Question1.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.*;
+
+public class Question1 {
+    /// push at the bottom of the stack
+    public static void pushAtBottom(Stack<Integer>  s, int data){
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+   
+    public static void main(String[] args) {
+      Stack<Integer> s = new Stack<>();
+      s.push(1);
+      s.push(2);
+      s.push(3);
+      s.push(4);
+
+      pushAtBottom(s,5);
+
+      while (!s.isEmpty()) {
+        System.out.println(s.pop());
+      }
+    }
+}
+
