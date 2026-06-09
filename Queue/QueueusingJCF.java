03a6035207d76b139156410abb3cc1e17a1a06a1
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-06-10 00:01:50 +0530

    Update QueueusingJCF.java

--- Queue/QueueusingJCF.java
+++ Queue/QueueusingJCF.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class QueueusingJCF {
      public static void main(String[] args) {
         //Queue q = new Queue();
-        Queue<Integer> q = new LinkedList<>();
+        //Queue<Integer> q = new LinkedList<>(); 
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
