7335a5636801fa36dd63c69f9020fd4ad73b3c36
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-06-18 15:46:42 +0530

    add

---
+++ Queue/deque.java
@@ -0,0 +1,21 @@
+package Queue;
+import java.util.*;
+
+public class deque {
+    public static void main(String[] args) {
+      Deque<Integer> deque = new LinkedList<>();
+      deque.addFirst(1);  
+      deque.addFirst(2);
+      System.out.println(deque);
+
+      deque.addLast(3);
+      deque.addLast(4);
+      System.out.print(deque);
+
+      deque.removeFirst();
+      System.out.println(deque);
+
+      System.out.println("First Element : " + deque.getFirst());
+      System.out.println("Last Element : " + deque.getLast());
+    }
+}
