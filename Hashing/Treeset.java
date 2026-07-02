385ad8edb2a45e5c63d5e4667b9f2aef6935463f
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-02 23:05:17 +0530

    Create Treeset.java

---
+++ Hashing/Treeset.java
@@ -0,0 +1,16 @@
+package Hashing;
+
+import java.util.TreeSet;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> set = new TreeSet<>();
+        set.add("delhi");
+        set.add("mumbai");
+        set.add("pune");
+        set.add("bhopal");
+        set.add("noida");
+
+        System.out.println(set);
+    }   
+}
