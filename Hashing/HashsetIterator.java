ea45c04612e11cf77504bcbc2a8c7a6961b19803
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-02 17:23:22 +0530

    Update HashsetIterator.java

--- Hashing/HashsetIterator.java
+++ Hashing/HashsetIterator.java
@@ -9,9 +9,16 @@ public class HashsetIterator {
         cities.add("Kolkata");
         cities.add("Chennai");
 
+        // using iterators
         Iterator it = cities.iterator();
         while (it.hasNext()) {
             System.out.println(it.next());
+        }
+System.out.println();
+
+        //using advanced for loop
+        for (String city : cities) {
+            System.out.println(city);
         }
 
     }
