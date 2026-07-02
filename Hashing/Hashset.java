fbab9fa95636a260372a57548176294d67e3e3ac
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-02 16:30:10 +0530

    Create Hashset.java

---
+++ Hashing/Hashset.java
@@ -0,0 +1,24 @@
+package Hashing;
+import java.util.HashSet;
+
+
+public class Hashset {
+
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet();
+
+        hs.add(1);
+        hs.add(2);
+        hs.add(3);
+        hs.add(4);
+        hs.add(1);
+
+        System.out.println(hs);
+
+        hs.remove(2);
+        System.out.println(hs.size());
+        if (hs.contains(2)) {
+            System.out.println(" set contains 2");
+        }
+    }
+}
