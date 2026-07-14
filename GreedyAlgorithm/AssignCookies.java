354a91e91edc8be1a4164ce1dabe34f5d9b08209
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-14 23:35:07 +0530

    Update AssignCookies.java

--- GreedyAlgorithm/AssignCookies.java
+++ GreedyAlgorithm/AssignCookies.java
@@ -4,24 +4,39 @@ import java.util.Arrays;
 
 public class AssignCookies {
 
-    public static int findContentChildren(int[] greed, int[] size) {
-        int n = greed.length;
-        int m = size.length;
-        int leftpointer = 0, rightpointer = 0;
+    // public static int findContentChildren(int[] greed, int[] size) {
+    //     int n = greed.length;
+    //     int m = size.length;
+    //     int leftpointer = 0, rightpointer = 0;
 
-        Arrays.sort(greed);
-        Arrays.sort(size);
+    //     Arrays.sort(greed);
+    //     Arrays.sort(size);
 
-        while(leftpointer < m && rightpointer < n){
-            if(greed[rightpointer] <= size[leftpointer]){
-                rightpointer = rightpointer+1;
-            }
-            leftpointer = leftpointer + 1;
+    //     while(leftpointer < m && rightpointer < n){
+    //         if(greed[rightpointer] <= size[leftpointer]){
+    //             rightpointer = rightpointer+1;
+    //         }
+    //         leftpointer = leftpointer + 1;
+    //     }
+
+    //     return rightpointer;
+    // }
+public static int findContentChildren(int[] greed, int[] size) {
+    Arrays.sort(greed);
+    Arrays.sort(size);
+
+    int child = 0;
+    int cookie = 0;
+
+    while (child < greed.length && cookie < size.length) {
+        if (size[cookie] >= greed[child]) {
+            child++;
         }
-
-        return rightpointer;
+        cookie++;
     }
 
+    return child;
+}
 
 
     public static void main(String[] args) {
