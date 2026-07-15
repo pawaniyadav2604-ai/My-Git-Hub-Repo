c76a355d3a49a6b1af0ddba108922261b28484ad
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-15 23:34:06 +0530

    Update PreorderTree.java

--- BinaryTree/PreorderTree.java
+++ BinaryTree/PreorderTree.java
@@ -27,12 +27,21 @@ public class PreorderTree {
             newNode.right = buildTree(nodes);
               return newNode;
         }
+
+        public static void preorder(Node root){
+            if (root == null) {
+                return;
+            }
+            System.out.println(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
             
         }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Binarytree tree = new Binarytree();
         Node root =tree.buildTree(nodes);
-        System.out.println(root.data);
+    tree.preorder(root);
     }
 }
