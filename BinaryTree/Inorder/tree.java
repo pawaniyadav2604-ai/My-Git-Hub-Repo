3f1b9e09a77527dd7d70bf9caec9044e8330d3ab
Pawani Yadav <pawaniyadav2604@gmail.com>
2026-07-16 20:01:51 +0530

    Create tree.java

---
+++ BinaryTree/Inorder/tree.java
@@ -0,0 +1,48 @@
+package BinaryTree.Inorder;
+
+public class tree {
+        static class Node {
+        int data;
+        Node left;
+        Node right;
+
+         Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+         }
+            
+        }
+
+        static class  Binarytree {
+        static int index = -1;
+        public static Node buildTree(int nodes[]){
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+              return newNode;
+        }
+
+        public static void Ineorder(Node root){
+            if (root == null) {
+                return;
+            }
+             Ineorder(root.left);
+            System.out.println(root.data + " ");
+           
+            Ineorder(root.right);
+        }
+            
+        }
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Binarytree tree = new Binarytree();
+        Node root =tree.buildTree(nodes);
+    tree.Ineorder(root);
+    }
+}
+
